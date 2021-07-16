d693dee94da31fd967feb6e0cf2216035da08c91
adriram99 <adriram99@gmail.com>
2021-07-16 01:05:57 -0400

    archivo inicial

---
+++ hips.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+# Trabajo Practico Sistemas Operativos 2 - HIPS
+# Autor: Adriram99
+
+#librerias
+import os
+import posix
+
+
+# Nombre:
+# Parametros:
+# Esta funcion verifica si hubieron modificaciones de los archivos binarios del sistema 
+# y los archivos /etc/passwd y /etc/shadow 
+
+def checkBPSfile():
+    #asdfgjshdf para probar el git
+    #mas cambios ah
