27b1f371bdd0efab7dd44cbd9f092212ce894e22
adriram99 <adriram99@gmail.com>
2021-07-17 14:58:19 -0400

    probando los md5sum y para crear base de datos

--- hips.py
+++ hips.py
@@ -6,13 +6,37 @@
 
 #librerias
 import os
+import subprocess
 import posix
-
+import psycopg2
+import hashlib
+import hips_bd
 
 # Nombre:
 # Parametros:
 # Esta funcion verifica si hubieron modificaciones de los archivos binarios del sistema 
 # y los archivos /etc/passwd y /etc/shadow 
-
-#def checkBPSfile():
+def checkBPSfile():
+    # flags para determinar si ya hubo una revision antes
+    fbin = 0;
+    fpasswd = 0;
+    fshadow = 0;
+    #control para /etc/passwd
+    md5 = os.system("md5sum /etc/passwd")
+    print(md5)
+    #en esta parte comparamos con el dato registrado en la bd
+    #control para /etc/shadow
+    md5 = os.system("sudo md5sum /etc/shadow")
+    print(md5)
+    #comparamos si hay cambios con respecto al ultimo registro en la bd
     
+
+# Funcion principal 
+def main():
+    #creamos la base de datos ejecutando el archivo .py
+  #os.system("python3 hips_bd.py")
+  checkBPSfile()
+
+
+if '__main__' == __name__:
+  main()
