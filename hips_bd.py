a72eb0d935549ec2cc93252e6838b7b4bfc0c8e8
adriram99 <adriram99@gmail.com>
2021-07-22 18:59:36 -0400

    probando Flask

--- hips_bd.py
+++ hips_bd.py
@@ -8,42 +8,24 @@ import subprocess
 # Parametros:
 # Esta funcion crea la base de datos que necesitamos para almacenar
 # algunas informaciones de comprobacion
-def createBD():
-    # establecemos la conexion
-    try:
-        connect = psycopg2.connect(database = "postgres", user = "postgres", password = " ")
-    except psycopg2.Error as e:
-        print('error')
-    #connect.autocommit = True
-
-    # creamos el objeto cursor
-    curr = connect.cursor()
-
-    # query para crear nueva base de datos
-    sql = "CREATE database hips;"
-
-    #creamos la BD
-    curr.execute(sql)
-    print("BD creado exitosamente")
-
-    #cerramos la conexion
-    connect.close()
 
 def create_table_BD():
     # establecemos conexion
     conn = psycopg2.connect(
-        database = "hips", user = "postgres", password = " ")
+        database = "hips", user = "postgres", password = "password")
     #conn.autocommit = True
 
     #creamos onjeto cursor
     curr = conn.cursor()
 
     # queries para creacion de tablas
-    sql = "CREATE TABLE passwd (md5_checksum varchar(32), date varchar(20); )"
+    sql = "CREATE TABLE passwd (md5_checksum varchar(32), date varchar(20));"
 
     #ejecutamos
     curr.execute(sql)
-    print("tabla creada exitosamente")
+    #print("tabla creada exitosamente")
 
     #cerramos conexion
     conn.close()
+
+create_table_BD()
---
+++ server.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/verificar-binarios/')
+def my_link():
+    print('I got clicked!')
+
+    return 'Click.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
