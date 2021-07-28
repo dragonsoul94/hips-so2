14c2663dd78c1761afae7158ae326371e05ecf7d
adriram <adriram99@gmail.com>
2021-07-28 13:30:30 -0400

    Creado el login, boton y avisos, funciones ok

--- hips.py
+++ hips.py
@@ -48,6 +48,15 @@ def connect_hipso2(option):
       return result
     except psycopg2.Error:
       print("ERROR.")
+  elif option == 2: #login
+    sql = '''SELECT * FROM login'''
+    try:
+      curr.execute(sql)
+      result = curr.fetchall()
+      #print(result)
+      return result
+    except psycopg2.Error:
+      print("Error")
   #cerramos la base de datos
   conn.close()
 
@@ -61,26 +70,66 @@ def compare_md5sum(md5p, cmd5p, md5s, cmd5s):
   #/etc/passwd
   temp = cmd5p[0]
   print(temp)
-  #print(cmd5p)
+  print(md5p)
   if temp != md5p:
     c = False
+    msg = ''
     msg = "/etc/passwd fue editado, no coinciden las md5sum."
-  #/etc/shadow
-  temp = cmd5s[0]
-  print(temp)
-  # print(cmd5s)
-  if temp != md5s:
-    c = False
-    msg = "/etc/shadow fue editado, no coinciden las md5sum."
+    #/etc/shadow
+    temp = cmd5s[0]
+    if temp != md5s:
+      c = False
+      msg = "/etc/passwd y /etc/shadow fueron editados, no coinciden las md5sum."
+
   return (c,msg)
+
+# Nombre: ipblock(ip)
+#
+# Esta funcion bloquea la ip recibida como parametro usando IPtables
+#
+def ipblock(ip):
+  p = subprocess.Popen("iptables -I INPUT -s "+ ip + "-j DROP", stdout=subprocess.PIPE,shell=True)
+  (out,err) = p.communicate()
+  p = subprocess.Popen("iptables -I INPUT -s "+ ip + "-j DROP", stdout=subprocess.PIPE,shell=True)
+  (out,err) = p.communicate() 
+
+# Nombre: quarantine(dir)
+#
+# Esta funcion envia a /tmp/quarantine el archivo que recibe como parametro
+#
+def quarantine(direc):
+  p = subprocess.Popen('mv ' + direc + ' /tmp/quarantine', stdout=subprocess.PIPE,shell=True)
+  (out,err) = p.communicate()
+
+# Nombre: alarmas_log(alarm_type, ip_source)
+#   
+#   Esta funcion agrega en /var/log/hips/alarmas.log las alertas generadas
+#   tiene como parametros el tipo de alerta y la ip fuente de la alarma
+def log_alarmas(alarm_type, ip_source):
+  # en caso de no contar con el ip 
+  if ip_source == '':
+    ip_source = 'NULL'
+  # anadimos fecha en formato DD/MM/AAAA
+  daytime = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+  alarm = daytime + "::" + alarm_type + "::" + ip_source
+  p = subprocess.Popen("sudo bash -c 'echo " + alarm + " >> /var/log/hips/alarmas.log'", stdout=subprocess.PIPE, shell=True)
+  (out, err) = p.communicate()
+
+# Nombre: log_prevencion(alarm_type, action)
+#   
+#   Esta funcion agrega en /var/log/hips/prevencion.log
+#   tiene como parametros el tipo de alerta y la accion preventiva realizada
+def log_prevencion(alarm_type, action):
+  daytime = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+  prev = daytime + "::" + alarm_type + "::" + action
+  p = subprocess.Popen("sudo bash -c 'echo " + prev + " >> /var/log/hips/prevencion.log'", stdout=subprocess.PIPE, shell=True)
+  (out, err) = p.communicate()
 
 # Nombre: check_md5sum_PS()
 #   
 #   Esta funcion revisa si se editaron los dir /etc/passwd y /etc/shadow a traves de md5sum
 #
 def check_md5sum_PS():
-  #p = subprocess.Popen("command", stdout=subprocess.PIPE, shell=True)
-  #(out, err) = p.comunicate()
   # calculamos el hash md5 de /etc/passwd
   p = subprocess.Popen('md5sum /etc/passwd', stdout=subprocess.PIPE, shell=True)
   (out,err) = p.communicate()
@@ -100,7 +149,7 @@ def check_md5sum_PS():
   cmd5s = consult[1]
   #comparamos
   (c, msg) = compare_md5sum(md5p, cmd5p, md5s, cmd5s)
-  if c == True:
+  if c == False:
     print("No coinciden")
     log_alarmas("md5sum diferente",'')
     send_mail_alert("md5sum diferente", msg)
@@ -145,45 +194,26 @@ def check_users_login():
   #enviamos al administrador
   send_mail_alert("IP de usuarios conectados", ip_conn)
   #para mostrar en la web
-  return ip_conn
-
-# Nombre: alarmas_log(alarm_type, ip_source)
-#   
-#   Esta funcion agrega en /var/log/hips/alarmas.log las alertas generadas
-#   tiene como parametros el tipo de alerta y la ip fuente de la alarma
-def log_alarmas(alarm_type, ip_source):
-  # en caso de no contar con el ip 
-  if ip_source == '':
-    ip_source = 'NULL'
-  # anadimos fecha en formato DD/MM/AAAA
-  daytime = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
-  print(daytime)
-  alarm = daytime + "::" + alarm_type + "::" + ip_source
-  print(alarm)
-  #os.system("echo " + alarm + " >> /var/log/hips/alarmas.log")
-  subprocess.Popen("sudo bash -c 'echo " + alarm + " >> /var/log/hips/alarmas.log'", stdout=subprocess.PIPE, shell=True)
-
-# Nombre: log_prevencion(alarm_type, action)
-#   
-#   Esta funcion agrega en /var/log/hips/prevencion.log
-#   tiene como parametros el tipo de alerta y la ip fuente de la alarma
-#def log_prevencion(alarm_type, action):
-
+  return "Registro enviado al correo del administrador."
 
 # Nombre: if_promis_mode()
 #
-#
-#
+# Esta funcion revisa si la interfaz del servidor actual esta en modo promiscuo y en los registros busca patrones
+# para otras interfaces (en caso de haber)
 def if_promis_mode():
+  #flag
+  status = 0
   #revisamos si el servidor esta actualmente en modo promiscuo
   p = subprocess.Popen('ip a show enp0s3 | grep -i promisc', stdout=subprocess.PIPE, shell=True)
   (out, err) = p.communicate()
   promis_on = out.decode('utf-8')
+  print("a")
   if promis_on != '':
+    status = 1
     # modo promiscuo 
     log_alarmas("NIC en modo promiscuo", '')
     #se informa al admin
-    #send_mail_alert("NIC en modo promiscuo", '')
+    send_mail_alert("NIC en modo promiscuo", '')
 
   #revisamos en el directorio /var/log/secure historial de comandos relacionados con el modo promiscuo
   #para ip link set [interface] promisc on/off
@@ -199,31 +229,30 @@ def if_promis_mode():
   pon = len(lon)
   poff = len(loff)
   if pon != poff and poff < pon:
+    status = 1
     #esta en modo promiscuo
     log_alarmas("NIC en modo promiscuo", '')
     #se informa al admin
-    #send_mail_alert("NIC en modo promiscuo", '')
+    send_mail_alert("NIC en modo promiscuo", '')
   # en teoria no puede comprobar si es que entro usando ifconfig [interface] promisc
-
-# Nombre: promis_apps()
-#
-#
-#
-#def promis_apps():
-
+  return status
 
 # Nombre: check_promis_mode_apps()
-#
-#
-#
+# 
+#  Funcion que realiza la inspeccion de apps que pueden poner el NIC en modo promiscuo
+#  o si el mismo se encuentra actualmente en ese estado
 def check_promis_mode_apps():
   #determinamos si el equipo entro en modo promiscuo
-  if_promis_mode()
+  st = if_promis_mode()
   #promis_apps()
-
+  if st == 1:
+    msg = "NIC en modo promiscuo"
+  else:
+    msg = "No se detecta que este en modo promiscuo el NIC del servidor"
+  return msg
 # Nombre: check_authentication_logs()
 #
-#
+# Revisa el dir /var/log/secure para controlar si hubieron errores de autenticacion, avisando al admin
 #
 def check_authentication_logs():
   #revisamos el dir /var/log/secure 
@@ -234,62 +263,87 @@ def check_authentication_logs():
   i = 0
   while i < laf:
     #se agregar a alarmas.log
-    log_alarmas("authentication failure", '')
+    log_alarmas("Error de autenticacion", '')
+    i+=1
   #se alerta al admin 
-  #send_mail_alert("Error de autenticacion",authf)
-
+  send_mail_alert("Error de autenticacion",authf)
+  if laf != 0:
+    msg = "Se detectaron errores de autenticacion. Revisar correo si es administrador"
+  else:
+    msg = "No se detectaron errores de autenticacion."
+  return msg
 # Nombre: check_failed_httpd_access()
 #
-#
+# Revisa el dir /var/log/httpd/access_log en busca de ingresos indebidos desde el servidor web
 #
 def check_failed_httpd_access():
   #extraemos nuestra ip de secret.ini
   config = configparser.ConfigParser()
   config.read('secret.ini')
   ip = config['CONFIG']['MY_IP']
+  msg = "No se encontraron errores de acceso a dir desc en servidor web"
   #revisamos el dir /var/log/httpd/access_log
   #grep -v -> busqueda inversa
-  p = subprocess.Popen('sudo cat /var/log/httpd/access_log | grep -v '+ ip + '| grep -v 127.0.0.1 | grep 404 ', stdout=subprocess.PIPE, shell=True)
+  p = subprocess.Popen('sudo cat /var/log/httpd/access_log | grep -v '+ ip + ' | grep -v "127.0.0.1" | grep "404" ', stdout=subprocess.PIPE, shell=True)
   (out,err) = p.communicate()
-  lip = out.decode('utf-8').splitlines()
+  sip = out.decode('utf-8')
+  lip = sip.splitlines()
   ips = []
+  mailb = "Errores de acceso a dir desconocidos en servidor web." 
   #extraemos las ip encontradas
   for line in lip:
-    ips.append(line.split(" ")[0])
+    i = line.split(" ")[0]
+    ips.append(i)
+    #alertamos
+    log_alarmas("Error de acceso a directorios desconocidos en servidor web",i)
   #bloqueamos las ip
-  #for i in ips:
+  for i in ips:
+    ipblock(ip)
+    mailb += i
+    log_prevencion("Error de acceso a directorios desconocidos en servidor web", "Bloqueo de IP usando IPtables")
+  #enviamos correo
+  if len(lip) != 0:
+    #por las alertas
+    send_mail_alert("Error de acceso a directorios desconocidos en servidor web", sip)
+    #por las medidas preventivas
+    send_mail_alert("Bloqueo de IP usando iptables", mailb)
+    msg = "Error de acceso a directorios desconocidos en servidor web. Bloqueo de IP usando iptables."
+  return msg
 
 # Nombre; check_tmp()
 #
-#
+# Esta funcion sirve para revisar el fichero /tmp en busca de shells y scripts sospechosos
 #
 def check_tmp():
   #revisamos si hay shells o scripts en el dir /tmp
-  #risk = ['sh', 'py', 'php', 'c', 'cpp', 'perl', 'exe', ]
-  cmd = 'find /tmp -type f -name "*.sh" -o -name "*.py" -o -name "*.php" -o -name "*.c" -o -name "*.cpp" -o -name "*.perl" -o -name "*.ruby"'
+  #risk = ['sh', 'py', 'php', 'c', 'cpp', 'perl', 'exe',  -o -name "*.py"]
+  cmd = 'find /tmp -type f -name "*.sh" -o -name "*.php" -o -name "*.c" -o -name "*.cpp" -o -name "*.perl" -o -name "*.ruby"'
   p = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True)
   (out, err) = p.communicate()
   shfiles = out.decode('utf-8')
+  msg = "No se encontraron scripts y shells en /tmp"
   if shfiles != '':
     qshells = []
     #hay posibles archivos de shell
     for line in shfiles.splitlines():
       qshells.append(line)
       #movemos los archivos en el dir /tmp/quarantine
-      #quarantine(line)
+      quarantine(line)
     #agregramos la alerta en alarms log
     log_alarmas("Shells y scripts encontrados en /tmp",'')
     send_mail_alert("Shells y scripts encontrados en /tmp",shfiles)
     #agregamos las medidas paliativas en prevencion log
-    #log_prevencion()
+    log_prevencion("Shells y scripts encontrados en /tmp", "Archivos puestos en cuarentena")
     send_mail_alert("archivos puestos en cuarentena",shfiles)
+    msg = "Shells y scripts encontrados en /tmp. Archivos puestos en cuarentena."
+  return msg
   #revisamos si hay scripts en dir /tmp
     #print(qshells)
 
 # Nombre: chech_failed_ssh()
 #
-#
-#
+# Revisa los failed pass via ssh y se avisa al admin, si un ip tiene 10 se bloquea
+# 
 def check_failed_ssh():
   
   config = configparser.ConfigParser()
@@ -306,6 +360,7 @@ def check_failed_ssh():
   fsshaf = 0 #autenthication failure
   fbip = 0 #block de ip
   mailb = ''
+  msg = "No se encontraron errores de contrasena via ssh."
   if len(lintruip) != 0:
     #hay failed passwd ssh
     for line in lintruip:
@@ -318,26 +373,22 @@ def check_failed_ssh():
     for i in ipl:
       if i in excess:
         excess[i] += 1
+        print(excess)
         if excess[i] == 10: #limite para bloquear ip
           fbip = 1
           mailb += i
-          #ip_block(i)
+          ipblock(i)
           #se agrega al log de prevencion
       else:
         excess[i] = 1
   if fsshaf == 1:
     #send mail
+    msg = "Errores de autenticacion SSH."
     send_mail_alert("Error de autenticacion SSH", intruip)
-  if fbip == 1:
-    send_mail_alert("Se bloqueo una o varias IPs", mailb)
-
-# Nombre: quarantine(dir)
-#
-#
-#
-def quarantine(direc):
-  p = subprocess.Popen('mv ' + direc + '/tmp/quarantine', stdout=subprocess.PIPE,shell=True)
-  (out,err) = p.communicate()
+    if fbip == 1:
+      msg += "Bloqueo de IPs."
+      send_mail_alert("Se bloqueo una o varias IPs", mailb)
+  return msg
 
 
 # Funcion principal 
@@ -347,9 +398,8 @@ def quarantine(direc):
   #check_promis_mode_apps()
   #check_authentication_logs()
   #check_failed_httpd_access()
-  #check_resources()
   #check_tmp()
   #check_failed_ssh()
 
-#if '__main__' == __name__:
-  #main()
+# if '__main__' == __name__:
+#   main()
--- server.py
+++ server.py
@@ -1,13 +1,71 @@
 #interfaz web HIPS
 #adriram
 
-from flask import Flask, render_template
+from email import message
+from flask import (
+    Flask,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for
+)
+import flask
+from flask.helpers import flash
+from flask_sqlalchemy import SQLAlchemy, _SQLAlchemyState
+import psycopg2
+import os
+import subprocess
+import configparser
 import hips
 
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'trabajo-practico-de-so2-hips-*'
+
+consult = hips.connect_hipso2(2)
+users = []
+for x in range(0,len(consult)):
+    users.append(User(consult[x][0],consult[x][1], consult[x][2]))
+print(users)
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        if username == '':
+            return redirect(url_for('login'))
+
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 # Para cargar mi plantilla html
-@app.route('/')
+@app.route('/inicio')
 def index():
     return render_template('index.html')
 
@@ -15,8 +73,43 @@ def index():
 def check_md5sum():
     result = hips.check_md5sum_PS()
     print(result)
-    return result
+    return render_template('index.html', message = result)
 
+@app.route('/check_users_login/')
+def check_users_login():
+    result = hips.check_users_login()
+    return render_template('index.html', message = result)
+
+@app.route('/check_promis_mode_apps/')
+def check_promis_mode_apps():
+    result = hips.check_promis_mode_apps()
+    return render_template('index.html', message = result)
+
+@app.route('/check_authentication_logs/')
+def check_authentication_logs():
+    result = hips.check_authentication_logs()
+    return render_template('index.html', message = result)
+
+@app.route('/check_failed_httpd_access/')
+def check_failed_httpd_access():
+    result = hips.check_failed_httpd_access()
+    return render_template('index.html', message = result)
+
+@app.route('/check_tmp/')
+def check_tmp():
+    result = hips.check_tmp()
+    return render_template('index.html', message = result)
+
+@app.route('/check_failed_ssh/')
+def check_failed_ssh():
+    result = hips.check_failed_ssh()
+    return render_template('index.html', message = result)
+
+@app.route('/logout/')
+def logout():
+    session.pop('user_id', None)
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
